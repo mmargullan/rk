cdab28bb7becb2fee49edab88b95324b48446d7a
Margulan <m.begzhan@aster.kz>
2024-12-15 23:28:36 +0500

    bug fix

--- src/main/kotlin/endterm/controller/UserController.kt
+++ src/main/kotlin/endterm/controller/UserController.kt
@@ -1,26 +1,16 @@
 package endterm.controller
 
-import com.google.gson.JsonArray
 import endterm.model.Dto.HttpMessage
 import endterm.model.User
-import endterm.service.RestTemplateService
 import endterm.service.UserService
-import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/user")
 class UserController(
-    private val restTemplateService: RestTemplateService,
-    private val userService: UserService,
+    private val userService: UserService
 ) {
-
-    val logger = LoggerFactory.getLogger(UserController::class.java)
 
     @PostMapping("/login")
     fun loginPlatonus(@RequestBody user: User): HttpMessage? {
